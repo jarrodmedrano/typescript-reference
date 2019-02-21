d947b9e0ed9bd2ffbb8f1d2b78d4d2b502f7a507
Jarrod Medrano <jmedran@gmail.com>
2019-02-20 20:46:37 -0700

    adding classes

--- classes.js
+++ classes.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Editor;
 (function (Editor) {
     Editor[Editor["MARVEL"] = 0] = "MARVEL";
@@ -10,10 +23,34 @@ var SuperHero = /** @class */ (function () {
         this.name = name;
         this.editor = editor;
         this.creationYear = creationYear;
+        //there is no such thing as a runtime private variable
+        //at runtime everything is accessible
+        //you can set variable to readonly
     }
+    //method of a class is by default public
+    SuperHero.prototype.createMessage = function () {
+        return "Super Hero: \n        " + this.name + " \n        " + Editor[this.editor] + " \n        " + this.creationYear;
+    };
     return SuperHero;
 }());
-var superman = new SuperHero('Superman', Editor.DC, 1938);
+var FlyingHero = /** @class */ (function (_super) {
+    __extends(FlyingHero, _super);
+    function FlyingHero() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    FlyingHero.prototype.fly = function (message) {
+        console.log(message);
+    };
+    return FlyingHero;
+}(SuperHero));
+//implicitly has a constructor
+var greenLantern = new FlyingHero('Silver Age Green Lantern', Editor.DC, 1959);
+console.log(greenLantern.createMessage());
+var superman = new FlyingHero('Superman', Editor.DC, 1938);
+superman.fly('up and away!');
+//this is valid
+superman.name = 'Spiderman';
 superman.name = 'Superman';
 superman.editor = Editor.DC;
 superman.creationYear = 1938;
+console.log(superman.createMessage());
--- classes.ts
+++ classes.ts
@@ -3,20 +3,52 @@ enum Editor {
     DC
 }
 
+
 class SuperHero {
     //implicitly add these three properties
-    constructor(public name: string,
-    public editor: Editor,
-    public creationYear: number) {
+    constructor (
+        public name: string,
+        public editor: Editor,
+        public creationYear: number
+    ) {
+        //there is no such thing as a runtime private variable
+        //at runtime everything is accessible
+        //you can set variable to readonly
+    }
 
+    //method of a class is by default public
+    createMessage() {
+        return `Super Hero: 
+        ${this.name} 
+        ${Editor[this.editor]} 
+        ${this.creationYear}`
     }
 }
 
-const superman = new SuperHero('Superman',Editor.DC,1938);
+
+class FlyingHero extends SuperHero {
+
+    fly (message:string) {
+        console.log(message);
+    }
+}
+
+//implicitly has a constructor
+const greenLantern = new FlyingHero('Silver Age Green Lantern', Editor.DC, 1959);
+
+console.log(greenLantern.createMessage());
+
+const superman = new FlyingHero('Superman',Editor.DC,1938);
+
+superman.fly('up and away!');
+//this is valid
+superman.name = 'Spiderman';
 
 superman.name = 'Superman';
 superman.editor = Editor.DC;
 superman.creationYear = 1938;
+
+console.log(superman.createMessage());
 
 
 
